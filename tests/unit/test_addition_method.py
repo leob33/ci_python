c6e9e0d5db3ebfb64263ae11058a28889b722314
leob33 <leo.babonnaud@yahoo.fr>
2022-04-05 14:11:17 +0200

    OPS : added gitlab-ci script TEST : added sample test

---
+++ tests/unit/test_addition_method.py
@@ -0,0 +1,25 @@
+from src.addition_method import addition_method
+
+
+def test_addition_method_given_two_integers():
+    # Given
+    a, b = 1, 2
+    expected_result = 3
+
+    # When
+    result = addition_method(a=a, b=b)
+
+    # Then
+    assert result == expected_result
+
+
+def test_addition_method_given_single_integer():
+    # Given
+    a = 1
+    expected_result = 1
+
+    # When
+    result = addition_method(a=a)
+
+    # Then
+    assert result == expected_result
